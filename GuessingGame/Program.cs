e380989836712769abd215cab4eee7e96dbaf16d
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:33:00 +0300

    WhileLoop

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int userGuess = Int32.Parse(Console.ReadLine());
+            bool correctGuess = false;
+
+            while (!correctGuess)
+            {
+                if(randomNumber == userGuess)
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("What is my number?");
+                    userGuess = Int32.Parse(Console.ReadLine());
+
+                }
+                
+            }
+
+
+        }
+    }
+}
